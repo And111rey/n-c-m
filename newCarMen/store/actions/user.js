a68133a5a8ef231edad8f936be0ca954f0087d37
Andrey <samu111ray@gmail.com>
2020-03-06 02:15:50 +0200

    LOGIN AND SIGNUP completed ....

--- newCarMen/store/actions/user.js
+++ newCarMen/store/actions/user.js
@@ -3,20 +3,35 @@ import firebase from "firebase"
 export const loginActionCreater = (email, password) => {
     return async (dispatch) => {
         try {
-            const response = await firebase.auth().signInWithEmailAndPassword(email, password)
-            //console.log(response.user.uid)
-            dispatch({type: "LOGIN", payload: response})
+            return await firebase.auth().signInWithEmailAndPassword(email, password)
+                .then((response) => {
+                    fetch(`https://car-magage.firebaseio.com/${response.user.uid}.json`,{
+                        method: "GET",
+                        headers: {"Content-Type": "aplication/json"}
+                    })
+                    dispatch({type: "LOGIN", payload: response.user.uid})
+                })
+            
         } catch (err) {alert(err)}
 
     }
 }
 
+
+
 export const loginSignUp = ({email, password, name}) => {
     return async (dispatch) => {
         try {
-            const response = await firebase.auth().createUserWithEmailAndPassword(email, password)
+            return await firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then((response) => {
+                    fetch(`https://car-magage.firebaseio.com/${response.user.uid}.json`,{
+                        method: "POST",
+                        headers: {"Content-Type": "aplication/json"},
+                        body: JSON.stringify({uid: response.user.uid, email})
+                    })
+                    dispatch({type: "SIGNUP", payload: response.user.uid})
+                })
                 .catch(function(error) {alert(error)});
-            dispatch({type: "SIGNUP", payload: response})
         } catch(err) {
             console.log(err)
         }
