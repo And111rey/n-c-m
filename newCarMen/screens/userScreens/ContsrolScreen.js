a7295a40eed6be50e5f1b4c6862fffa5c79bd36d
Andrey <samu111ray@gmail.com>
2020-03-09 15:13:00 +0200

    COOL

--- newCarMen/screens/userScreens/ContsrolScreen.js
+++ newCarMen/screens/userScreens/ContsrolScreen.js
@@ -1,14 +1,30 @@
 import React, {useState, useEffect} from 'react';
-
 import {styles} from "../../style"
+
+
 import {StyleSheet, ScrollView, Text, View, Button, TextInput, onChangeText, TouchableWithoutFeedback, TouchableOpacity , Keyboard } from 'react-native';
 
 export const ControlScreen = ({route, navigation}) => {
-  console.log("___CONTROLL SCREEN___  ", route.params.data)
-const { carName,  } = route.params.data
+  console.log("___CONTROLL SCREEN___  ", route.params.data.selectedFunctions)
+const { carName, selectedFunctions } = route.params.data
+console.log("part of props______>>>>> ", selectedFunctions)
+
+
+  const elements = selectedFunctions.map((el) => {
+    let nameElem = Object.keys(el)
+    return (
+      <TouchableOpacity style={styles.button} >
+        <Text> {nameElem}</Text>
+      </TouchableOpacity>
+    )
+  })
+
   return (
         <View style={styles.container}  >
             <Text>{carName}</Text>
+            <View>
+              {elements}
+            </View>
         </View>
   );
 }
